045fb69ab56d8d400967d49e9be2d8ed5a8d82b0
Tatuolkinuora <ag3462@student.jamk.fi>
2025-04-28 11:49:53 +0300

    about

--- speedepeli/src/routes/about/+page.svelte
+++ speedepeli/src/routes/about/+page.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
-  import { goto } from '$app/navigation';
+	import { goto } from '$app/navigation';
 </script>
 
 <h1>About sivu päivitellään jossain kohtaa</h1>
+<img
+	src="https://media.tenor.com/yheo1GGu3FwAAAAM/rick-roll-rick-ashley.gif"
+	alt="Rick Roll"
+	width="300"
+	height="200"
+/>
