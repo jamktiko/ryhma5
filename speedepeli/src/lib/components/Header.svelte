d3fcd89b82d5a975d38659322bc7bc8b2cc09fb5
teemu, jalava <ag2353@student.jamk.fi>
2025-04-28 10:34:25 +0300

    lisattiin oikea fontti ja logo headeriin

--- speedepeli/src/lib/components/Header.svelte
+++ speedepeli/src/lib/components/Header.svelte
@@ -3,14 +3,23 @@
 </script>
 
 <header>
-<h1>SpeedepeliPro</h1>
+<h1>Speede Game Pro</h1>
 </header>
 
 <style>
+  @import url('https://fonts.googleapis.com/css2?family=Jersey+10&display=swap');
 h1{
   color: #ff3e00;
   font-size: 2em;
   text-align: center;
   margin: 0;
+  font-family: 'Jersey 10';
+font-style: normal;
+font-weight: 400;
+font-size: 128px;
+line-height: 137px;
+letter-spacing: 0.02em;
+color: #000000;
+text-shadow: 0px 4px 9.3px rgba(255, 0, 0, 0.25);
 }
 </style>
