7bb13c8eaf0446076dd318ba1374a98317c69ba0
Aapo Hampaala <ag3652@student.jamk.fi>
2025-04-28 10:16:48 +0300

    footer testailua

---
+++ speedepeli/src/lib/components/Footer.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+let { company, year }: { company: string; year?: number } = $props();
+</script>
+<footer>
+	Copyright by {company}, {year}
+</footer>
--- speedepeli/src/routes/+layout.svelte
+++ speedepeli/src/routes/+layout.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
 import Header from '$lib/components/Header.svelte';
+import Footer from '$lib/components/Footer.svelte';
 </script>
 <Header text="SpeedepeliPRo"/>
+<main></main>
+<Footer company="AOTT Oy"/>
