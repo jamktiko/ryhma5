636caaddf5f0972b61bc38694b90191855556f08
Aapo Hampaala <ag3652@student.jamk.fi>
2025-04-28 10:20:42 +0300

    footer muokkauksia

--- speedepeli/src/routes/+layout.svelte
+++ speedepeli/src/routes/+layout.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 import Header from '$lib/components/Header.svelte';
 import Footer from '$lib/components/Footer.svelte';
+let year = new Date().getFullYear();
 </script>
 <Header text="SpeedepeliPRo"/>
 <main></main>
-<Footer company="AOTT Oy"/>
+<Footer company="AOTT Oy" year={year}/>
