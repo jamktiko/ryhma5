634650c0f47256363dd1589e428fe716cff1c5ed
Tatuolkinuora <ag3462@student.jamk.fi>
2025-04-29 09:40:02 +0300

    game-over modaali

--- speedepeli/src/lib/components/GameOver.svelte
+++ speedepeli/src/lib/components/GameOver.svelte
@@ -2,16 +2,18 @@
   import Modal from "./Modal.svelte";
   interface Props {
 		hideModal: () => void;
+		score: number;
 	}
 
-	let {hideModal }: Props = $props();
+	let {hideModal, score }: Props = $props();
 </script>
 <Modal>
 	{#snippet header()}
 		<h1>Game Over</h1>
 	{/snippet}
-	<p>Score:</p>
+	<p>Score: {score}</p>
 	{#snippet footer()}
 		<button onclick={hideModal}>Sulje</button>
+		<button onclick={() => { hideModal(); }}>Restart</button>
 	{/snippet}
 </Modal>
