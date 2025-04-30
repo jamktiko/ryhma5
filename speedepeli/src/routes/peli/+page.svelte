43b45dd81b078c291e49296af1c54640e4b4396c
Aapo Hampaala <ag3652@student.jamk.fi>
2025-04-30 10:17:19 +0300

    bug fix highscore

--- speedepeli/src/routes/peli/+page.svelte
+++ speedepeli/src/routes/peli/+page.svelte
@@ -22,7 +22,7 @@
 	let gameOver = false; // peli päättynyt -tilamuuttuja
 	let clickedThisRound = false; // tarkistaa, onko pelaaja klikannut väriä tällä kierroksella
 	let showModal = $state(false); // näyttääkö pelin päättymisen jälkeen modalin
-	let highscoreList: number[] = []; // highscore lista
+	let highscoreList: number[] = $state([]); // highscore lista
 
 	//countdown muuttujat
 	let countdownValue = $state(3); // aloituslaskuri
