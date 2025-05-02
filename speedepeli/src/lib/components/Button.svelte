e2284086415a2383d73f314b7cdc0e0e40a4bf04
teemu, jalava <ag2353@student.jamk.fi>
2025-05-02 09:38:56 +0300

    buttonien gradient muokkaus

--- speedepeli/src/lib/components/Button.svelte
+++ speedepeli/src/lib/components/Button.svelte
@@ -21,13 +21,14 @@
 		{#if keyLabel}
 		<div style=
 		"background: linear-gradient(180deg, rgba(255, 255, 255, 0.38) 24.46%, rgba(196, 196, 196, 0.27) 75.59%, rgba(93, 93, 93, 0.18) 99.52%);
-		box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.63);
-/* Note: backdrop-filter has minimal browser support */
+		box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.63);
+		/* Note: backdrop-filter has minimal browser support */
+			display:block
 			width: 250px;
     	height: 250px;
 			border-radius: 50%;
-    border: none;
-    margin: 8px;
+    	border: none;
+   	 	margin: 0px;
 			/* Note: backdrop-filter has minimal browser support */">
 			<span class="key-label">{keyLabel}</span></div>
 		{/if}
