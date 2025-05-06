6efe3637d6e34512baa87c04565b7d14b2f20153
teemu, jalava <ag2353@student.jamk.fi>
2025-05-06 09:51:16 +0300

    aloitus-sivu napin muutokset

--- speedepeli/src/lib/components/Header.svelte
+++ speedepeli/src/lib/components/Header.svelte
@@ -10,7 +10,7 @@
 	<h1>{text}</h1>
 	{#if $page.url.pathname !== '/' && !showModal2}
 		<nav>
-			<a href="/" class:is-active={$page.url.pathname === '/'}>Aloitus sivu</a>
+			<a href="/" class="custom-button">Aloitus sivu</a>
 
 			<!-- <button onclick={() => (showAbout = true)}>About</button>  -->
 		</nav>
@@ -37,33 +37,52 @@
 		color: #000000;
 		text-shadow: 0px 4px 9.3px rgba(255, 0, 0, 0.25);
 	}
-	nav {
-		position: flex;
-		width: 120px;
-		height: 50px;
-		left: 52px;
-		top: 60px;
-		box-sizing: border-box;
 
-		/* starttinappi tyyli
-
-starttinappityyli
-*/
-		background: linear-gradient(180deg, #ff6161 29.81%, #d55151 70.19%, #993a3a 90.87%);
-		border: 1px solid #000000;
-		box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-		border-radius: 14px;
-		font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-		font-size: x-large;
+	@import url('https://fonts.googleapis.com/css2?family=Jersey+10&display=swap');
+	.custom-button {
+		background: linear-gradient(180deg, #ff6161 0%, #d55151 50%, #993a3a 100%);
+		border: 3px solid #000000;
+		border-radius: 8px;
+		color: black;
+		font-family: 'Jersey 10', sans-serif;
+		font-size: 20px;
+		padding: 5px 10px;
+		text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.25);
+		box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.3);
+		cursor: pointer;
+		transition: all 0.2s ease;
+		position: fixed;
+		display: flex;
+		top: 100px;
 	}
+
+	.custom-button:hover {
+		background: linear-gradient(180deg, #ff7777 0%, #e66666 50%, #aa4444 100%);
+		transform: translateY(-2px);
+		box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.4);
+	}
+
+	.custom-button:active {
+		background: linear-gradient(180deg, #d55151 0%, #993a3a 50%, #772222 100%);
+		transform: translateY(1px);
+		box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.3);
+	}
+
+	
 	@media (max-width: 850px) {
 		h1 {
 			font-size: 100px;
+		}
+		.custom-button{
+			font-size: 10px;
 		}
 	}
 	@media (max-width: 700px) {
 		h1 {
 			font-size: 70px;
+		}
+		.custom-button {
+			font-size: 5px;
 		}
 	}
 </style>
