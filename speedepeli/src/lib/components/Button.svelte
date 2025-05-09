736076096d7d355c4116d71857b53c6f69d3d0d0
Tatuolkinuora <ag3462@student.jamk.fi>
2025-05-09 12:30:55 +0300

    buttoniin lisätty reunat kun aktiivinen

--- speedepeli/src/lib/components/Button.svelte
+++ speedepeli/src/lib/components/Button.svelte
@@ -5,7 +5,9 @@
 <div class="button-wrapper">
 	<button
 		class="game-button"
-		style="background-color: {color}; opacity: {active ? 1 : 0.5};"
+		style="background-color: {color}; opacity: {active ? 1 : 0.5}; border: 4px solid {active
+			? 'orange'
+			: 'transparent'};"
 		onclick={onClick}
 		aria-label="Interactive button"
 	>
@@ -26,11 +28,15 @@
 		width: clamp(180px, 20vw, 280px);
 		height: clamp(180px, 20vw, 280px);
 		border-radius: 100%;
-		border-color:none;
-		background: linear-gradient(180deg, rgba(255, 255, 255, 0.34) 50.78%, rgba(196, 196, 196, 0.36) 60.58%, rgba(93, 93, 93, 0.22) 77.88%);
+		border-color: none;
+		background: linear-gradient(
+			180deg,
+			rgba(255, 255, 255, 0.34) 50.78%,
+			rgba(196, 196, 196, 0.36) 60.58%,
+			rgba(93, 93, 93, 0.22) 77.88%
+		);
 		box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.63);
 		backdrop-filter: blur(4.3px);
-
 	}
 
 	.game-button {
@@ -46,10 +52,8 @@
 		align-items: center;
 		box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
 		text-decoration: none;
-		
 	}
 	.game-button:hover {
-	
 		transform: translateY(1px);
 		box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.4);
 	}
@@ -60,17 +64,16 @@
 		box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.3);
 	}
 
-
 	.key-label {
 		font-family: 'Jersey 10', sans-serif;
 		font-size: clamp(80px, 8vw, 150px);
 		color: grey;
 	}
 
-
 	/* Responsive adjustments for different screen sizes */
 	@media (max-width: 1024px) {
-		.button-wrapper, .game-button {
+		.button-wrapper,
+		.game-button {
 			width: clamp(120px, 22vw, 200px);
 			height: clamp(120px, 22vw, 200px);
 		}
@@ -81,14 +84,15 @@
 	}
 
 	@media (max-width: 768px) {
-		.button-wrapper, .game-button {
+		.button-wrapper,
+		.game-button {
 			width: clamp(80px, 20vw, 150px);
 			height: clamp(80px, 20vw, 150px);
 			flex-direction: row;
 		}
 		.game-button {
 			width: 100%;
-			height:100%;
+			height: 100%;
 		}
 
 		.key-label {
@@ -96,17 +100,18 @@
 			display: none;
 		}
 		.game-button:hover {
-	box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.3);
-	transform: none;
-}
-.game-button:active {
-		background: linear-gradient(180deg, #72727247 0%, #8e8e8e1f 50%, #8b8b8b24 100%);
-		box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.3);
-	}
+			box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.3);
+			transform: none;
+		}
+		.game-button:active {
+			background: linear-gradient(180deg, #72727247 0%, #8e8e8e1f 50%, #8b8b8b24 100%);
+			box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.3);
+		}
 	}
 
 	@media (max-width: 480px) {
-		.button-wrapper, .game-button {
+		.button-wrapper,
+		.game-button {
 			width: clamp(120px, 22vw, 200px);
 			height: clamp(120px, 22vw, 200px);
 			flex-direction: column;
@@ -117,16 +122,17 @@
 			display: none;
 		}
 		.game-button:hover {
-	box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.3);
-	transform: none;
-}
-.game-button:active {
-		background: linear-gradient(180deg, #72727247 0%, #8e8e8e1f 50%, #8b8b8b24 100%);
-		box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.3);
-	}
+			box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.3);
+			transform: none;
+		}
+		.game-button:active {
+			background: linear-gradient(180deg, #72727247 0%, #8e8e8e1f 50%, #8b8b8b24 100%);
+			box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.3);
+		}
 	}
 	@media (max-width: 320px) {
-		.button-wrapper, .game-button {
+		.button-wrapper,
+		.game-button {
 			width: clamp(100px, 15vw, 100px);
 			height: clamp(100px, 15vw, 100px);
 			flex-direction: column;
@@ -137,13 +143,12 @@
 			display: none;
 		}
 		.game-button:hover {
-	box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.3);
-	transform: none;
-}
-.game-button:active {
-		background: linear-gradient(180deg, #72727247 0%, #8e8e8e1f 50%, #8b8b8b24 100%);
-		box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.3);
+			box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.3);
+			transform: none;
+		}
+		.game-button:active {
+			background: linear-gradient(180deg, #72727247 0%, #8e8e8e1f 50%, #8b8b8b24 100%);
+			box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.3);
+		}
 	}
-	}
-
 </style>
